588c053e81e44011c59ade67be42328b656f6db6
jk960903 <jk960903@naver.com>
2022-01-09 20:54:07 +0900

    rebase merge

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("first master");
 
+        System.out.println("issue1");
         System.out.println("second master");
     }
 }
